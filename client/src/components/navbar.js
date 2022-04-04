ce796c52d641bc1b7a1a57e8f40c7484d93ccfcb
jhuset2003 <jhusetqp@gmail.com>
2022-04-04 10:10:19 -0500

    Subiendo info

---
+++ client/src/components/navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { NavLink } from "react-router-dom";
+ 
+
+export default function Navbar() {
+ return (
+   <div>
+     <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+       <NavLink className="navbar-brand" to="/">
+       <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="white" class="bi bi-card-checklist" viewBox="0 0 16 16">
+  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+  <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
+</svg>       </NavLink>
+       <button
+         className="navbar-toggler"
+         type="button"
+         data-toggle="collapse"
+         data-target="#navbarSupportedContent"
+         aria-controls="navbarSupportedContent"
+         aria-expanded="false"
+         aria-label="Toggle navigation"
+       >
+         <span className="navbar-toggler-icon"></span>
+       </button>
+ 
+       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul className="navbar-nav ml-auto">
+           <li className="nav-item">
+             <NavLink className="nav-link" to="/create">
+             <button type="button" class="btn btn-dark">Crear Tarea</button>
+             </NavLink>
+           </li>
+         </ul>
+       </div>
+     </nav>
+   </div>
+ );
+}
